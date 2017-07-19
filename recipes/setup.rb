1897769903e943cf3d7bbdc649e405b0656cc648
Brook Riggio <brook@decielo.com>
2017-07-19 22:06:34 +0000

    Add tested cookbook config with templates.

---
+++ recipes/setup.rb
@@ -0,0 +1,11 @@
+package "tree" do
+  action :install
+end
+
+template '/etc/motd' do
+  source 'motd.erb'
+  mode '0644'
+  owner 'root'
+  group 'root'
+end
+
---
+++ test/recipes/default_test.rb
@@ -0,0 +1,26 @@
+# # encoding: utf-8
+
+# Inspec test for recipe workstation::default
+
+# The Inspec reference, with examples and extensive documentation, can be
+# found at https://docs.chef.io/inspec_reference.html
+
+unless os.windows?
+  describe user('root') do
+    it { should exist }
+  end
+end
+
+describe port(80) do
+  it { should_not be_listening }
+end
+
+describe package('tree') do
+  it { should be_installed }
+end
+
+describe file('/etc/motd') do
+  it { should be_owned_by 'root' }
+  its('mode') { should cmp '0644' }
+  its('content') { should match /\d+\.\d+\.\d+\.\d+/ }
+end
